622be9df796949e51caf919d1e6806a4c33aac55
timoruth02 <134884874+timoruth02@users.noreply.github.com>
2023-08-12 00:04:35 -0500

    S

--- ServerSpace/src/main/java/EntityLayer/InfraestructuraEntity.java
+++ ServerSpace/src/main/java/EntityLayer/InfraestructuraEntity.java
@@ -1,13 +1,104 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package EntityLayer;
+
+import java.util.Date;
 
 /**
  *
  * @author RUTH
  */
 public class InfraestructuraEntity {
+
+    private int InfraestructuraId;
+    private int TipoInfraestructuraId;
+    private String Estado;
+    private String Codigo;
+    private String Descripcion;
+    private double Ancho;
+    private String Largo;
+    private String Altura;
+    private String Nivel;
+    private int Aforo;
     
+    public int getInfraestructuraId() {
+        return InfraestructuraId;
+    }
+
+    public void setInfraestructuraId(int InfraestructuraId) {
+        this.InfraestructuraId = InfraestructuraId;
+    }
+
+    public int getTipoInfraestructuraId() {
+        return TipoInfraestructuraId;
+    }
+
+    public void setTipoInfraestructuraId(int TipoInfraestructuraId) {
+        this.TipoInfraestructuraId = TipoInfraestructuraId;
+    }
+
+    public String getEstado() {
+        return Estado;
+    }
+
+    public void setEstado(String Estado) {
+        this.Estado = Estado;
+    }
+
+    public String getCodigo() {
+        return Codigo;
+    }
+
+    public void setCodigo(String Codigo) {
+        this.Codigo = Codigo;
+    }
+
+    public String getDescripcion() {
+        return Descripcion;
+    }
+
+    public void setDescripcion(String Descripcion) {
+        this.Descripcion = Descripcion;
+    }
+
+    public double getAncho() {
+        return Ancho;
+    }
+
+    public void setAncho(double Ancho) {
+        this.Ancho = Ancho;
+    }
+
+    public String getLargo() {
+        return Largo;
+    }
+
+    public void setLargo(String Largo) {
+        this.Largo = Largo;
+    }
+
+    public String getAltura() {
+        return Altura;
+    }
+
+    public void setAltura(String Altura) {
+        this.Altura = Altura;
+    }
+
+    public String getNivel() {
+        return Nivel;
+    }
+
+    public void setNivel(String Nivel) {
+        this.Nivel = Nivel;
+    }
+
+    public int getAforo() {
+        return Aforo;
+    }
+
+    public void setAforo(int Aforo) {
+        this.Aforo = Aforo;
+    }
+
+ 
+
 }
