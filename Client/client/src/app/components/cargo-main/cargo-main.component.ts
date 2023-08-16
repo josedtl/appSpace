eed3624014542af02ef7fb428b3e5face667c232
timoruth02 <134884874+timoruth02@users.noreply.github.com>
2023-08-16 00:20:51 -0500

    s

--- Client/client/src/app/components/cargo-main/cargo-main.component.ts
+++ Client/client/src/app/components/cargo-main/cargo-main.component.ts
@@ -1,6 +1,6 @@
 import { Component , OnInit, HostBinding, ViewChild, ElementRef} from '@angular/core';
 import { CargoService } from '../../services/cargo.service';
-import {  CargoEntity} from '../../models/CargoEntity';
+import {  CargoEntity } from '../../models/CargoEntity';
 
 @Component({
   selector: 'app-cargo-main',
@@ -10,11 +10,28 @@ import {  CargoEntity} from '../../models/CargoEntity';
 export class CargoMainComponent {
   
   CargoItems: any = [];;
+  @ViewChild('saveModal') saveModal!: ElementRef; // Referencia a la ventana emergente
   constructor(private service:CargoService) {
   }
+  newItem: CargoEntity = {
+    tipoInfraestructuraId: 0,
+    nombre: '',
+    fechaRegistro: new Date(),
+    codUsuario: 'adm ',
+    estadoRegistro: true,
+  //  action: 0,
+  };
+  
 
   ngOnInit() {
     this.getCargo();
+  }
+  openModal() {
+   // this.newItem.action = 1;
+    this.saveModal.nativeElement.style.display = 'block';
+  }
+  closeModal() {
+    this.saveModal.nativeElement.style.display = 'none';
   }
 
   getCargo() {
