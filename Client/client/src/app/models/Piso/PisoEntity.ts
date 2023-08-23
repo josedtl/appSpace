170cee157feb298cae19be4f0a89b7d17e2df594
timoruth02 <134884874+timoruth02@users.noreply.github.com>
2023-08-23 01:18:21 -0500

    s

---
+++ Client/client/src/app/models/Piso/PisoEntity.ts
@@ -0,0 +1,11 @@
+export interface PisoEntity {
+    Item: number,
+    PisoId: number,
+    Codigo?: string,
+    Valor?:number,
+    FechaRegistro?: Date,
+    CodUsuario?: string,
+    EstadoRegistro?: boolean,
+    Action?: number,
+    Seleccion:boolean
+};
