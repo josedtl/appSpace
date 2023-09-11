0f80545d97fa9d29b283baf01e318c0cb7bacba3
josedtl <30784219+josedtl@users.noreply.github.com>
2023-09-11 18:31:26 -0500

    d

--- ServerSpace/src/main/java/Business/Cargo.java
+++ ServerSpace/src/main/java/Business/Cargo.java
@@ -2,29 +2,57 @@ package Business;
 
 import DataLayer.CargoDB;
 import EntityLayer.CargoEntity;
+import Framework.Inj;
 import Models.CargoItemModel;
 import java.util.ArrayList;
 
-public class Cargo  extends  Business.MyCode.Cargo{
+public class Cargo extends Business.MyCode.Cargo {
 
     public ArrayList<CargoEntity> GetAllItems() {
-        CargoDB BD = new CargoDB();
-        return BD.GetAllItems();
+
+        try {
+            CargoDB BD = new CargoDB();
+            Inj.IniciarTranssaccionConsulta();
+            return BD.GetAllItems();
+        } catch (Exception ex) {
+            System.out.println("ERROR " + ex);
+            throw new UnsupportedOperationException("MENSAJE :" + ex);
+        }
     }
 
     public ArrayList<CargoEntity> GetAllItem(int Id) {
-        CargoDB BD = new CargoDB();
-        return BD.GetAllItem(Id);
+        try {
+            CargoDB BD = new CargoDB();
+            Inj.IniciarTranssaccionConsulta();
+            return BD.GetAllItem(Id);
+        } catch (Exception ex) {
+            System.out.println("ERROR " + ex);
+            throw new UnsupportedOperationException("MENSAJE :" + ex);
+        }
+
     }
 
     public CargoEntity Save(CargoEntity Item) {
-        CargoDB BD = new CargoDB();
-        return BD.Save(Item);
+
+        try {
+            CargoDB BD = new CargoDB();
+            Inj.IniciarTranssaccionConsulta();
+            return BD.Save(Item);
+        } catch (Exception ex) {
+            System.out.println("ERROR " + ex);
+            throw new UnsupportedOperationException("MENSAJE :" + ex);
+        }
     }
 
     public Boolean Delete(int Id) {
-        CargoDB BD = new CargoDB();
-        return BD.Delete(Id);
+        try {
+            CargoDB BD = new CargoDB();
+            Inj.IniciarTranssaccionConsulta();
+            return BD.Delete(Id);
+        } catch (Exception ex) {
+            System.out.println("ERROR " + ex);
+            throw new UnsupportedOperationException("MENSAJE :" + ex);
+        }
     }
 
 }
--- ServerSpace/src/main/java/Business/MyCode/PersonaNatural.java
+++ ServerSpace/src/main/java/Business/MyCode/PersonaNatural.java
@@ -1,14 +1,20 @@
-
 package Business.MyCode;
 
 import DataLayer.PersonaNaturalDB;
 import EntityLayer.PersonaNaturalEntity;
+import Framework.Inj;
 import java.util.ArrayList;
 
 public class PersonaNatural {
-  
-     public ArrayList<PersonaNaturalEntity> GetPersonaNaturalMainItems() {
-        PersonaNaturalDB BD = new PersonaNaturalDB();
-        return BD.GetPersonaNaturalMainItems();
+
+    public ArrayList<PersonaNaturalEntity> GetPersonaNaturalMainItems() {
+        try {
+            PersonaNaturalDB BD = new PersonaNaturalDB();
+            Inj.IniciarTranssaccionConsulta();
+            return BD.GetPersonaNaturalMainItems();
+        } catch (Exception ex) {
+            System.out.println("ERROR " + ex);
+            throw new UnsupportedOperationException("MENSAJE :" + ex);
+        }
     }
 }
