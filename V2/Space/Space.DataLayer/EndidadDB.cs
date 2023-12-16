3913855bf3d9cd090a9f9972a4a14edac927864c
josedtl <30784219+josedtl@users.noreply.github.com>
2023-12-16 01:38:29 -0500

    cambio

--- V2/Space/Space.DataLayer/EndidadDB.cs
+++ V2/Space/Space.DataLayer/EndidadDB.cs
@@ -4,68 +4,73 @@ using Datos.Conexion;
 using Space.EntityLayer;
 
 
-namespace Space.DataLayer;
-public class EndidadDB
+namespace Space.DataLayer
 {
-    public List<EntidadEntity> GetEndidad()
+
+    public class EndidadDB
     {
-        List<EntidadEntity> ListaData = new List<EntidadEntity>();
-        DatabaseManager dbManager = new DatabaseManager();
-        SqlDataReader reader = dbManager.StoreConsulta("sp_PersonaAll");
-        EntidadEntity Item = null;
-        while (reader.Read())
+        public List<EntidadEntity> GetEndidad()
         {
-            Item = null;
-            Item = new EntidadEntity();
+            List<EntidadEntity> ListaData = new List<EntidadEntity>();
+            DatabaseManager dbManager = new DatabaseManager();
+            SqlDataReader reader = dbManager.StoreConsulta("sp_PersonaAll");
+            EntidadEntity Item = null;
+            while (reader.Read())
+            {
+                Item = null;
+                Item = new EntidadEntity();
 
-            Item.EntidadId = Convert.ToInt32(reader["EntidadId"]);
-            Item.NumDocumento = reader["NumDocumento"].ToString();
-            Item.Nombres = reader["Nombres"].ToString();
-            Item.ApellidoPaterno = reader["ApellidoPaterno"].ToString();
-            Item.ApellidoMaterno = reader["ApellidoMaterno"].ToString();
-            Item.FechaNacimiento = Convert.ToDateTime(reader["FechaNacimiento"]);
-            Item.Direccion = reader["Direccion"].ToString();
-            Item.Telefono = reader["Telefono"].ToString();
-            Item.Correo = reader["Correo"].ToString();
-            Item.SexoId = Convert.ToInt32(reader["SexoId"].ToString());
-            Item.EstadoCivilId = Convert.ToInt32(reader["EstadoCivilId"].ToString());
+                Item.EntidadId = Convert.ToInt32(reader["EntidadId"]);
+                Item.NumDocumento = reader["NumDocumento"].ToString();
+                Item.Nombres = reader["Nombres"].ToString();
+                Item.ApellidoPaterno = reader["ApellidoPaterno"].ToString();
+                Item.ApellidoMaterno = reader["ApellidoMaterno"].ToString();
+                Item.FechaNacimiento = Convert.ToDateTime(reader["FechaNacimiento"]);
+                Item.Direccion = reader["Direccion"].ToString();
+                Item.Telefono = reader["Telefono"].ToString();
+                Item.Correo = reader["Correo"].ToString();
+                Item.SexoId = Convert.ToInt32(reader["SexoId"].ToString());
+                Item.EstadoCivilId = Convert.ToInt32(reader["EstadoCivilId"].ToString());
 
-            ListaData.Add(Item);
+                ListaData.Add(Item);
+            }
+
+            reader.Close();
+
+            return ListaData;
         }
 
-        reader.Close();
-
-        return ListaData;
-    }
-
-    public EntidadEntity Save(EntidadEntity Ent)
-    {
-        DatabaseManager dbManager = new DatabaseManager();
-
-        String NameStore = "sp_LineaNaviera_Save";
-        if (Ent.EntidadId > 0) NameStore = "sp_LineaNaviera_Update";
-
-
-        SqlParameter[] parametros = new SqlParameter[]
+        public EntidadEntity Save(EntidadEntity Ent)
         {
-            new SqlParameter("@LineaNavieraId", Ent.EntidadId)
+            DatabaseManager dbManager = new DatabaseManager();
+
+            String NameStore = "up_Entidad_Insert";
+            if (Ent.EntidadId > 0) NameStore = "up_Entidad_UpdateByKey";
+
+
+            SqlParameter[] parametros = new SqlParameter[]
+            {
+            new SqlParameter("@EntidadId", Ent.EntidadId)
             {
                 Direction= ParameterDirection.InputOutput,
                 SqlDbType= SqlDbType.Int
             },
-            new SqlParameter("@Nombre", Ent.NumDocumento),
-            new SqlParameter("@Color", Ent.Nombres),
-            new SqlParameter("@Codigo", Ent.ApellidoPaterno),
-            new SqlParameter("@CodUsuario",Ent.ApellidoMaterno),
+            new SqlParameter("@TipoEntidadId", Ent.NumDocumento),
+            new SqlParameter("@TipoDocumentoIdentidadId", Ent.Nombres),
+            new SqlParameter("@NumDocumento", Ent.ApellidoPaterno),
+            new SqlParameter("@NombreCompleto",Ent.ApellidoMaterno),
             new SqlParameter("@FechaRegistro", Ent.FechaNacimiento),
+            new SqlParameter("@CodUsuario",Ent.cod),
             new SqlParameter("@EstadoRegistro", Ent.Direccion),
-        };
+            };
 
 
-        Ent.EntidadId = dbManager.ProcedimientoSave(NameStore, parametros, "@LineaNavieraId");
+            Ent.EntidadId = dbManager.ProcedimientoSave(NameStore, parametros, "@LineaNavieraId");
 
 
-        return Ent;
+            return Ent;
 
+        }
     }
+
 }
--- V2/Space/Space.EntityLayer/EntidadEntity.cs
+++ V2/Space/Space.EntityLayer/EntidadEntity.cs
@@ -2,47 +2,47 @@ using System.Text.Json.Serialization;
 
 namespace Space.EntityLayer
 {
-	public partial class EntidadEntity : Utilitario
-	{
+    public partial class EntidadEntity : Utilitario
+    {
 
-		public EntidadEntity()
-			: base()
-		{
-			EntidadId = GetInt32Default();
-			NumDocumento = GetStringDefault();
-			Nombres = GetStringDefault();
-			ApellidoPaterno = GetStringDefault();
-			ApellidoMaterno = GetStringDefault();
-			FechaNacimiento = GetDateTimeDefault();
-			Direccion = GetStringDefault();
-			Telefono = GetStringDefault();
-			Correo = GetStringDefault();
-			SexoId = GetInt32Default();
-			EstadoCivilId = GetInt32Default();
-		}
-		[JsonPropertyName("EntidadId")]
-		public Int32 EntidadId { get; set; }
-		[JsonPropertyName("NumDocumento")]
-		public String NumDocumento { get; set; }
-		[JsonPropertyName("Nombres")]
-		public String Nombres { get; set; }
-		[JsonPropertyName("ApellidoPaterno")]
-		public String ApellidoPaterno { get; set; }
-		[JsonPropertyName("ApellidoMaterno")]
-		public String ApellidoMaterno { get; set; }
-		[JsonPropertyName("FechaNacimiento")]
-		public DateTime FechaNacimiento { get; set; }
-		[JsonPropertyName("Direccion")]
-		public String Direccion { get; set; }
-		[JsonPropertyName("Telefono")]
-		public String Telefono { get; set; }
-		[JsonPropertyName("Correo")]
-		public String Correo { get; set; }
-		[JsonPropertyName("SexoId")]
-		public Int32 SexoId { get; set; }
-		[JsonPropertyName("EstadoCivilId")]
-		public Int32 EstadoCivilId { get; set; }
+        public EntidadEntity()
+            : base()
+        {
+            this.EntidadId = GetInt32Default();
+            this.TipoEntidadId = GetInt32Default();
+            this.TipoDocumentoIdentidadId = GetInt32Default();
+            this.NumDocumento = GetStringDefault();
+            this.NombreCompleto = GetStringDefault();
+            this.FechaRegistro = GetDateTimeDefault();
+            this.CodUsuario = GetStringDefault();
+            this.UbigeoId = GetInt32Default();
+        }
 
-	}
+        [JsonPropertyName("EntidadId")]
+        public Int32 EntidadId { get; set; }
+
+        [JsonPropertyName("TipoEntidadId")]
+
+        public Int32 TipoEntidadId { get; set; }
+
+        [JsonPropertyName("TipoDocumentoIdentidadId")]
+        public Int32 TipoDocumentoIdentidadId { get; set; }
+
+        [JsonPropertyName("NumDocumento")]
+        public String NumDocumento { get; set; }
+
+        [JsonPropertyName("NombreCompleto")]
+        public String NombreCompleto { get; set; }
+
+        [JsonPropertyName("FechaRegistro")]
+        public DateTime FechaRegistro { get; set; }
+
+        [JsonPropertyName("CodUsuario")]
+        public String CodUsuario { get; set; }
+
+        [JsonPropertyName("UbigeoId")]
+        public Int32 UbigeoId { get; set; }
+
+    }
 }
 
