f55db058517a7d845190dbb5f59279fd6d3befba
josedtl <30784219+josedtl@users.noreply.github.com>
2024-05-31 00:00:58 -0500

    Version 1.3
    Add

--- Facturacion/PruebaFACT/AppXML/Program.cs
+++ Facturacion/PruebaFACT/AppXML/Program.cs
@@ -12,26 +12,35 @@ namespace AppXML
     {
         static void Main(string[] args)
         {
-
-
-            var people = new SalonEntity
+            var ItemFactura = new Invoice
             {
-                Grado = "3",
-                Seccion = "B",
-                Profe = new ProfesorEntity { Name = "Ruth", Apellido = "Medina" },
-                DetalleAlumnos = new List<DetalleAlumnos> { new DetalleAlumnos
-                {
-                      Alumnos = new List<AlumnoEntity>
-            {
-                new AlumnoEntity { Name = "Juan",Apellido = "Ramirez" },
-               new AlumnoEntity { Name = "Maria",Apellido = "Timoteo" },
-                  new AlumnoEntity { Name = "Josue",Apellido = "Merino" },
-                     new AlumnoEntity { Name = "Pedro",Apellido = "Carrasco" },
-                        new AlumnoEntity { Name = "Liz",Apellido = "Tomas" },
-            }
-                } }
+
+                UBLVersionID = "2.1",
+                CustomizationID = "2.0",
+                ID = "FFF1-1",
+                IssueDate = DateTime.Now,
+                //IssueTime=new DateTime()
 
             };
+
+            //var people = new SalonEntity
+            //{
+            //    Grado = "3",
+            //    Seccion = "B",
+            //    Profe = new ProfesorEntity { Name = "Ruth", Apellido = "Medina" },
+            //    DetalleAlumnos = new List<DetalleAlumnos> { new DetalleAlumnos
+            //    {
+            //          Alumnos = new List<AlumnoEntity>
+            //{
+            //    new AlumnoEntity { Name = "Juan",Apellido = "Ramirez" },
+            //   new AlumnoEntity { Name = "Maria",Apellido = "Timoteo" },
+            //      new AlumnoEntity { Name = "Josue",Apellido = "Merino" },
+            //         new AlumnoEntity { Name = "Pedro",Apellido = "Carrasco" },
+            //            new AlumnoEntity { Name = "Liz",Apellido = "Tomas" },
+            //}
+            //    } }
+
+            //};
 
             // Especificar la carpeta de destino
             string folderPath = @"D:\XML";
@@ -43,13 +52,13 @@ namespace AppXML
             }
 
             // Especificar el nombre del archivo
-            string filePath = Path.Combine(folderPath, "SALON2.xml");
+            string filePath = Path.Combine(folderPath, "Factura.xml");
 
             // Serializar el objeto a XML y guardarlo en el archivo
-            XmlSerializer serializer = new XmlSerializer(typeof(SalonEntity));
+            XmlSerializer serializer = new XmlSerializer(typeof(Invoice));
             using (StreamWriter writer = new StreamWriter(filePath))
             {
-                serializer.Serialize(writer, people);
+                serializer.Serialize(writer, ItemFactura);
             }
 
             Console.WriteLine("Archivo XML generado y guardado en " + filePath);
@@ -58,46 +67,46 @@ namespace AppXML
 
 
     }
-    [Serializable]
-    public class ProfesorEntity
-    {
-        public string Name { get; set; }
-        public string Apellido { get; set; }
-    }
+    //[Serializable]
+    //public class ProfesorEntity
+    //{
+    //    public string Name { get; set; }
+    //    public string Apellido { get; set; }
+    //}
 
-    [Serializable]
-    public class AlumnoEntity
-    {
-        [XmlElement("Person")]
-        public string Name { get; set; }
-        public string Apellido { get; set; }
-    }
+    //[Serializable]
+    //public class AlumnoEntity
+    //{
+    //    [XmlElement("Person")]
+    //    public string Name { get; set; }
+    //    public string Apellido { get; set; }
+    //}
 
-    [Serializable]
-    public class DetalleAlumnos
-    {
-        [XmlElement("Alumnos")]
-        public List<AlumnoEntity> Alumnos { get; set; }
-    }
-
-
-    [Serializable]
-    public class SalonEntity
-    {
-        [XmlElement("Grado")]
-        public String Grado { get; set; }
-        [XmlElement("Seccion")]
-        public String Seccion { get; set; }
+    //[Serializable]
+    //public class DetalleAlumnos
+    //{
+    //    [XmlElement("Alumnos")]
+    //    public List<AlumnoEntity> Alumnos { get; set; }
+    //}
 
 
-        [XmlElement("Profesor")]
-        public ProfesorEntity Profe { get; set; }
+    //[Serializable]
+    //public class SalonEntity
+    //{
+    //    [XmlElement("Grado")]
+    //    public String Grado { get; set; }
+    //    [XmlElement("Seccion")]
+    //    public String Seccion { get; set; }
 
 
-        [XmlElement("DetalleAlumnos")]
-        public List<DetalleAlumnos> DetalleAlumnos { get; set; }
+    //    [XmlElement("Profesor")]
+    //    public ProfesorEntity Profe { get; set; }
 
 
-    }
+    //    [XmlElement("DetalleAlumnos")]
+    //    public List<DetalleAlumnos> DetalleAlumnos { get; set; }
+
+
+    //}
 
 }
