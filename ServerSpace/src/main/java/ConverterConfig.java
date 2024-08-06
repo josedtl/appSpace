fe6567b34c9692b107ec5b37cad6f47c9ce5144e
josedtl <30784219+josedtl@users.noreply.github.com>
2024-08-05 22:20:16 -0500

    Version 1.0
    Add

--- ServerSpace/src/main/java/ConverterConfig.java
+++
@@ -1,9 +0,0 @@
-
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-public class ConverterConfig {
-	@Value("${path.to.prop.name }")
-	private String myProp;
-}
